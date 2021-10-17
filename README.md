ac8cde84ebc827ac086bd74bc6905f99b5d257d8
Soukaina Bensaad <bensaad.soukaina@emsi-edu.ma>
2021-10-17 22:12:24 +0100

    First Version of the Spichly app with main functions

---
+++ README.md
@@ -0,0 +1,57 @@
+## Breakly
+
+Spichly is a Text-to-Speech application made using javascript and the Speech Synthesis API.
+
+- [Demo Website Spichly](https://nagisoft.github.io/spichly/)
+
+## Screenshots
+
+### Screenshots
+
+![Screenshot](./spichly.gif)
+
+## Features
+
+Features Done :
+
+- [x] Text To Speech using different voices and languages
+- [x] Resume/Pause
+- [x] Animated visualizer on Logo
+- [x] Volume and Speed controls
+- [x] About (Little ? on the right bottom of the page)
+- [x] Browser Support
+
+Todo :
+
+- [ ] Improve App on page change
+- [ ] Speech to MP3 Download
+- [ ] Read Text from Uploaded Files
+
+## Built With
+
+- HTML/CSS
+- Vanilla JavaScript + JQuery
+- Speech Synthesis API
+
+## Author
+
+**Bensaad Soukaina**
+
+- [Linkedin](https://www.linkedin.com/in/soukaina-bensaad/ "Linkedin")
+
+## Ressources
+
+I got some design inspiration on some elements from Codepen (Linked in file Comments) :
+
+- [Fontawesome](https://www.fontawesome.com/ "Fontawesome")
+- [Codepen](https://codepen.io/trending "Codepen")
+
+## Support
+
+Contributions, issues, and feature requests are welcome!
+
+Give a ⭐️ if you like this project!
+
+## License
+
+Distributed under the GNU AGPLv3 License. See `LICENSE` File for more information.
---
+++ script.js
@@ -0,0 +1,109 @@
+/*
+ * Check for browser support
+ */
+var supportMsg = document.getElementById('msg');
+
+if ('speechSynthesis' in window) {
+	supportMsg.innerHTML = 'Your browser <strong>supports</strong> speech synthesis.';
+} else {
+	supportMsg.innerHTML = 'Sorry your browser <strong>does not support</strong> speech synthesis.';
+}
+
+
+// Get all the elements
+var button = document.getElementById('speak');
+var pauseButton = document.getElementById('pause');
+var resumeButton = document.getElementById('resume');
+var speechMsgInput = document.querySelector('textarea');
+var voiceSelect = document.getElementById('voice');
+var volumeInput = document.getElementById('volume');
+var speed = document.getElementById('rate');
+
+
+// get voices and languages list and put them in select option
+function loadVoices() {
+   /* if (String(window.performance.getEntriesByType("navigation")[0].type)) {
+    	speechSynthesis.cancel();
+
+	}	*/
+ 
+	console.log(String(window.performance.getEntriesByType("navigation")[0].type));
+	var voices = speechSynthesis.getVoices();
+  
+	voices.forEach(function(voice, i) {
+		var option = document.createElement('option');
+		option.value = voice.name;
+		option.innerHTML = voice.name;
+		  
+		voiceSelect.appendChild(option);
+	});
+}
+
+loadVoices();
+
+window.speechSynthesis.onvoiceschanged = function(e) {
+  loadVoices();
+};
+
+
+
+function speak(text) {
+	if(window.speechSynthesis.paused){
+		window.speechSynthesis.resume();
+	}
+	else{
+	var msg = new SpeechSynthesisUtterance();
+  
+	msg.text = text;
+  
+	msg.volume = parseFloat(volumeInput.value);
+	msg.rate = parseFloat(speed.value);
+  
+
+	if (voiceSelect.value) {
+		msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == voiceSelect.value; })[0];
+	}
+  
+	window.speechSynthesis.speak(msg);
+
+	msg.onstart = function() { 
+		addAnimation()
+ 	};
+	msg.onresume = function() { 
+		addAnimation()
+ 	};
+	 msg.onend = function() { 
+		removeAnimation()
+ 	};
+	  msg.onpause = function() { 
+		removeAnimation()
+ 	};
+}
+}
+function addAnimation(){
+	$('.visualizer-1').css( 'animation', 'music 0.5s linear infinite');
+		$('.visualizer-2').css( 'animation' , "music 0.9s linear infinite");
+		$('.visualizer-3').css( 'animation' , "music 0.7s linear infinite");
+		$('.visualizer-4').css( 'animation' , "music 1.1s linear infinite");
+}
+
+function removeAnimation(){
+	$('.visualizer-item').css( 'animation' , "none");
+}
+
+button.addEventListener('click', function(e) {
+	if (speechMsgInput.value.length > 0) {
+		speak(speechMsgInput.value);
+	}
+});
+
+pauseButton.addEventListener('click', function(e) {
+		window.speechSynthesis.pause();
+
+});
+
+resumeButton.addEventListener('click', function(e) {
+		window.speechSynthesis.resume();
+
+});
+
